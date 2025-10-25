e0f97a1039b30ffa7bd6dee5acf9cc6788302dd3
saltanovas <zumbass@gmail.com>
2025-10-26 00:03:30 +0300

    disable shadow

--- .config/osx/screenshot.sh
+++ .config/osx/screenshot.sh
@@ -6,6 +6,6 @@ defaults write com.apple.screencapture name -string "Screenshot"
 defaults write com.apple.screencapture include-date -bool true
 # Other options: jpg, pdf, psd, gif, tga, tiff, bmp, heic
 defaults write com.apple.screencapture type -string png
-defaults write com.apple.screencapture disable-shadow -bool false
+defaults write com.apple.screencapture disable-shadow -bool true
 mkdir -p "$HOME/Desktop/s" && defaults write com.apple.screencapture location -string "~/Desktop/s"
 defaults write com.apple.screencapture show-thumbnail -bool true
