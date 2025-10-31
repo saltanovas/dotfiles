5324a2a22cfd27106e7f087695ef97a3d169987b
saltanovas <zumbass@gmail.com>
2025-11-01 01:46:04 +0200

    feat: Add script to start screen saver

---
+++ .config/raycast/scripts/screensaver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Screen Saver
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🖼️
+
+# Documentation:
+# @raycast.description Starts screen saver
+# @raycast.author saltanovas
+
+open -a ScreenSaverEngine
