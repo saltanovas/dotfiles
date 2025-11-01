a2be941973a6262059b723422471ed37e9610a14
saltanovas <zumbass@gmail.com>
2025-11-01 21:31:27 +0200

    feat: Change folder name for screenshots

--- .config/osx/screenshot.sh
+++ .config/osx/screenshot.sh
@@ -7,5 +7,5 @@ defaults write com.apple.screencapture include-date -bool true
 # Other options: jpg, pdf, psd, gif, tga, tiff, bmp, heic
 defaults write com.apple.screencapture type -string png
 defaults write com.apple.screencapture disable-shadow -bool true
-mkdir -p "$HOME/Desktop/s" && defaults write com.apple.screencapture location -string "~/Desktop/s"
+mkdir -p "$HOME/Desktop/ss" && defaults write com.apple.screencapture location -string "$HOME/Desktop/ss"
 defaults write com.apple.screencapture show-thumbnail -bool true
