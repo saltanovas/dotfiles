1567c6571654a5235ed2a09bfa46c64d4c10e3c4
saltanovas <zumbass@gmail.com>
2025-11-01 03:09:55 +0200

    cleanup

--- .config/osx/hid.sh
+++ .config/osx/hid.sh
@@ -2,56 +2,36 @@
 
 set -euo pipefail
 
-# Natural scrolling | Mimics touch devices
+# Sensitivity
+defaults write NSGlobalDomain com.apple.trackpad.scaling -float 0.6875
+defaults write NSGlobalDomain com.apple.mouse.scaling -float 1.0
+# Scroll speed
+defaults write NSGlobalDomain com.apple.trackpad.scrolling -float 0.1838
+defaults write NSGlobalDomain com.apple.scrollwheel.scaling -float 0.3125
+# Enable inertia when scrolling
+defaults write com.apple.AppleMultitouchTrackpad TrackpadMomentumScroll -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseMomentumScroll -bool true
+# Double click speed
+# Affects both mouse and trackpad
+defaults write NSGlobalDomain com.apple.mouse.doubleClickThreshold -float 1.1
+
+# Enable mouse acceleration
+defaults write NSGlobalDomain com.apple.mouse.linear -bool true
+
+# Enable natural scrolling
+# Mimics touch devices
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
 
 # Enable instant spring loading for directories or apps in dock when hovering files over them
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
-
-
-# TODO: cleanup below
-defaults write com.apple.AppleMultitouchTrackpad TrackpadMomentumScroll -bool true
-
-
-defaults write NSGlobalDomain com.apple.scrollwheel.scaling -float 0.3125
-defaults write NSGlobalDomain com.apple.trackpad.scrolling -float 0.1838
-
-# ========== Drag ==========
-# Tap → tap-and-hold | Cannot release a finger
-defaults write com.apple.AppleMultitouchTrackpad Dragging -bool true
-# Tap → tap-and-hold | Can release a finger, tap again to release
-defaults write com.apple.AppleMultitouchTrackpad DragLock -bool false
-# Drag with three finger
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
-
-
-    "com.apple.driver.AppleBluetoothMultitouch.trackpad" =     {
-        HIDScrollZoomModifierMask = 0;
-        TrackpadHandResting = 1;
-        TrackpadThreeFingerDrag = 0;
-    };
-
+# Keep trackpad enabled when mouse is connected
+defaults write com.apple.AppleMultitouchTrackpad USBMouseStopsTrackpad -bool false
 
 # #######################################
-# Mouse
+# Mouse gestures
 # #######################################
-# Sensitivity
-defaults write NSGlobalDomain com.apple.mouse.scaling -float 1.0
-# Acceleration
-defaults write NSGlobalDomain com.apple.mouse.linear -bool true
-
-# ========== Scroll ==========
-# Speed
-defaults write NSGlobalDomain com.apple.scrollwheel.scaling -float 0.4412
-# Use inertia when scrolling
-defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseMomentumScroll -bool true
-
-# ========== Click ==========
-# Double click speed
-defaults write NSGlobalDomain com.apple.mouse.doubleClickThreshold -float 1.1
-
 # ========== Secondary click ==========
 # OneButton = off | TwoButton = right click | TwoButtonSwapped = left click
 defaults write NSGlobalDomain MouseButtonMode -string TwoButton
@@ -76,16 +56,7 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseTwoFingerHor
 # #######################################
 # Trackpad
 # #######################################
-# Sensitivity
-defaults write NSGlobalDomain com.apple.trackpad.scaling -float 0.6875
-defaults write NSGlobalDomain com.apple.trackpad.scrolling -float 0.3125
-
-# Do not disable trackpad when mouse is connected
-defaults write com.apple.AppleMultitouchTrackpad USBMouseStopsTrackpad -bool false
-
 # ========== Click ==========
-# Tap to click
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 # Click pressure
 # 0 = light | 1 = medium | 2 = firm
 defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 1
@@ -110,23 +81,6 @@ defaults write NSGlobalDomain com.apple.trackpad.forceClick -bool true
 defaults write com.apple.AppleMultitouchTrackpad TrackpadTwoFingerDoubleTapGesture -bool true
 # Tap three fingers | Look up & data detectors
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerTapGesture -bool false
-
-# ========== Pinch, Spread & Rotate ==========
-# Two fingers | Zoom in or out
-defaults write com.apple.AppleMultitouchTrackpad TrackpadPinch -bool true
-# Two fingers | Rotate
-defaults write com.apple.AppleMultitouchTrackpad TrackpadRotate -bool true
-# Four fingers
-# 2 = Launchpad & Desktop
-defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerPinchGesture -int 2
-# Five fingers
-# 2 = Launchpad & Desktop
-defaults write com.apple.AppleMultitouchTrackpad TrackpadFiveFingerPinchGesture -int 2
-
-# Pinch | Launchpad
-defaults write com.apple.dock showLaunchpadGestureEnabled -bool true
-# Spread | Desktop
-defaults write com.apple.dock showDesktopGestureEnabled -bool true
 
 # ========== Swipe horizontally ==========
 # Two fingers | Between pages
@@ -153,3 +107,30 @@ defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerVertSwipeGest
 defaults write com.apple.dock showMissionControlGestureEnabled -bool true
 # Swipe down | App Exposé
 defaults write com.apple.dock showAppExposeGestureEnabled -bool true
+
+# ========== Pinch, Spread & Rotate ==========
+# Two fingers | Zoom in or out
+defaults write com.apple.AppleMultitouchTrackpad TrackpadPinch -bool true
+# Two fingers | Rotate
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRotate -bool true
+# Four fingers
+# 2 = Launchpad & Desktop
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerPinchGesture -int 2
+# Five fingers
+# 2 = Launchpad & Desktop
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFiveFingerPinchGesture -int 2
+
+# Pinch | Launchpad
+defaults write com.apple.dock showLaunchpadGestureEnabled -bool true
+# Spread | Desktop
+defaults write com.apple.dock showDesktopGestureEnabled -bool true
+
+# ========== Drag ==========
+# Enable drag with three fingers
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+
+# Disable tap-and-hold drag
+# Tap → tap-and-hold | Cannot release a finger
+defaults write com.apple.AppleMultitouchTrackpad Dragging -bool false
+# Tap → tap-and-hold | Can release a finger, tap again to release
+defaults write com.apple.AppleMultitouchTrackpad DragLock -bool false
