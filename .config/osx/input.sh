7281f11bda847e2d44bfeaffe2a04c9df0dca1a2
saltanovas <zumbass@gmail.com>
2025-11-01 21:49:51 +0200

    feat: Format input settings

--- .config/osx/input.sh
+++ .config/osx/input.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# ========================================
-# Input
-# ========================================
-# ===== Autocorrect =====
+# ========== Autocorrect ==========
 # Disable auto-correct spelling
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # Disable auto-capitalization
@@ -12,18 +9,28 @@ defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 # Disable full stop with double-space
 defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
-# Disable smart quotes
+# Disable smart quote substitution
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-# Disable smart dashes
+# Disable smart dashe substitution
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-# ===== Dictation =====
+# Disable text completion
+defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticTextCompletionCollapsed -bool false
+# Disable inline text prediction
+defaults write NSGlobalDomain NSAutomaticInlinePredictionEnabled -bool false
+# Enable automatic spelling check by language
+defaults write NSGlobalDomain NSSpellCheckerAutomaticallyIdentifiesLanguages -bool true
+
+# ========== Dictation ==========
 # Disable dictation
 defaults write com.apple.HIToolbox AppleDictationAutoEnable -bool false
-# ===== Input Sources =====
-# Show Input menu in menu bar
-defaults write com.apple.TextInputMenu visible -bool true
+
+# ========== Input Sources ==========
+# Disable input source indicator popup
+defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled 0
 # Enable per-context input source switching (each app keeps its own active keyboard layout)
 defaults write com.apple.HIToolbox AppleGlobalTextInputProperties -dict TextInputGlobalPropertyPerContextInput -bool true
+
 # Remove all input sources
 defaults delete com.apple.HIToolbox AppleEnabledInputSources
 # Add ABC input source
