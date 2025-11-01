772bab70514f59391db896d7051a34965eb77fdc
saltanovas <zumbass@gmail.com>
2025-11-01 02:25:03 +0200

    chore: Update comment

--- .config/osx/appearance.sh
+++ .config/osx/appearance.sh
@@ -11,6 +11,7 @@ defaults write NSGlobalDomain AppleReduceDesktopTinting -bool false
 # Show scrollbars only when scrolling
 defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
 
-# Set sidebar icon size, affects Finder, Settings sidebars etc.
+# Set sidebar icon size
+# Affects sidebars on Finder, System Settings etc.
 # 1 = small | 2 = medium | 3 = large
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
