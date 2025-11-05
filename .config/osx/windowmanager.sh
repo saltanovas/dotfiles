4564f60c20b3ffc63ca95fc10b5eb2b743909cde
saltanovas <zumbass@gmail.com>
2025-11-05 05:57:27 +0200

    feat: Configure window tabbing mode and window restoration behavior

--- .config/osx/windowmanager.sh
+++ .config/osx/windowmanager.sh
@@ -2,6 +2,15 @@
 
 set -euo pipefail
 
+# Prefer tabs only in full screen
+# 1. always | 2. manual (Never) | 3. fullscreen
+defaults write NSGlobalDomain AppleWindowTabbingMode -string fullscreen
+# Never restore windows or tabs when reopening apps after quit
+# Note: this does not affect the macOS "Reopen windows when logging back in" feature
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+# Always ask to keep changes when closing documents
+defaults write NSGlobalDomain NSCloseAlwaysConfirmsChanges -bool true
+
 # Enable dragging windows to screen edges to tile them
 defaults write com.apple.WindowManager EnableTilingByEdgeDrag -bool true
 # Enable dragging windows to the menu bar to fill the screen
