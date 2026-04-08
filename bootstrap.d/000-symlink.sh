a8a8f74b2df5b65c0a75fd492a8c937e44cf8fc8
saltanovas <zumbass@gmail.com>
2026-04-09 02:57:15 +0300

    Backup destination when creating symlinks

--- bootstrap.d/000-symlink.sh
+++ bootstrap.d/000-symlink.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 symlink() {
-    local src="$1"
-    local dest="$2"
+    local src="$1" dest="$2"
 
     if [[ -z "$src" || -z "$dest" ]]; then
         err "Source and destination must be provided to create a symlink.";
@@ -11,28 +10,27 @@ symlink() {
 
     if [[ ! -e "$src" ]]; then
         err "Cannot create a symlink, source does not exist: $src";
-        return 1∏
+        return 1
     fi
 
-    # -e follows symlinks and checks if the target exists; thus, this catch and override dangling symlinks
-    if [[ ! -e "$dest" ]]; then
-        ln -sf "$src" "$dest" || return 1
-        ohai "Symlink created: $src -> $dest";
-        return 0
+    # -e follows symlinks and checks if the target exists; returns false for broken symlinks
+    if [[ -e "$dest" || -L "$dest" ]]; then
+        [[ "$(realpath "$src")" = "$(realpath "$dest")" ]] && return 0
+        ohai "Destination already exists: $dest. Backing up to $dest.bak"
+        mv "$dest" "$dest.bak"
     fi
 
-    if [[ "$(realpath "$src")" = "$(realpath "$dest")" ]]; then return 0; fi
-
-    err "Cannot create symlink, destination already exists: $src -> $dest"
+    ln -sfn "$src" "$dest" || return 1
+    ohai "Symlink created: $src -> $dest"
 }
 
 ohai "Creating config symlinks..."
 
 symlink "$DOTFILES_ROOT/.config/zsh/.zshenv" "$HOME" || exit 1
-
 # Prettier walks upward all the way to filesystem root; no XDG file path support
-symlink "$DOTFILES_ROOT/.config/.prettier/.prettierrc.yaml" "$HOME" || exit 1
+symlink "$DOTFILES_ROOT/.config/prettier/.prettierrc.yaml" "$HOME" || exit 1
+symlink "$DOTFILES_ROOT/.config/jetbrains/.settings.json" "$HOME/Library/Application Support/JetBrains/Toolbox"
 
-symlink "$DOTFILES_ROOT/.config/.jetbrains/.settings.json" "$HOME/Library/Application Support/JetBrains/Toolbox"
-
-for config in .config/*; do symlink "$DOTFILES_ROOT/$config" "$HOME/$config" || exit 1; done
+for config in .config/*; do
+    symlink "$DOTFILES_ROOT/$config" "$HOME/$config" || exit 1;
+done
