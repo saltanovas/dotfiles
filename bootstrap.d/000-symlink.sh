8a7d1311540991058ee2ab34734d94630792b083
saltanovas <zumbass@gmail.com>
2026-04-20 08:23:59 +0300

    Improve symlink creation logic

--- bootstrap.d/000-symlink.sh
+++ bootstrap.d/000-symlink.sh
@@ -15,15 +15,17 @@ symlink() {
         return 1
     fi
 
+    mkdir -p "$dest"
+    local target="$dest/$(basename "$src")"
+
     # -e follows symlinks and checks if the target exists; returns false for broken symlinks
-    if [[ -e "$dest" ]]; then
-        [[ "$(realpath "$src")" = "$(realpath "$dest")" ]] && return 0
-        ohai "Destination already exists: $dest. Creating backup."
-        backup "$dest"
+    if [[ -e "$target" ]] && [[ "$(realpath "$src")" != "$(realpath "$target")" ]]; then
+        ohai "Destination already exists: $target. Creating backup..."
+        backup "$target" || return 1
     fi
 
-    ln -sfn "$src" "$dest" || return 1
-    ohai "Symlink created: $src -> $dest"
+    ln -sfn "$src" "$target" || return 1
+    ohai "Symlink created: $src -> $target"
 }
 
 backup() {
@@ -41,7 +43,7 @@ backup() {
     mv "$target" "$backup"
 }
 
-ohai "Creating config symlinks..."
+ohai "Creating symlinks..."
 
 symlink "$DOTFILES_ROOT/.config/zsh/.zshenv" "$HOME" || exit 1
 # Prettier walks upward all the way to filesystem root; no XDG file path support
@@ -49,8 +51,7 @@ symlink "$DOTFILES_ROOT/.config/prettier/.prettierrc.yaml" "$HOME"
 symlink "$DOTFILES_ROOT/.config/jetbrains/.settings.json" "$HOME/Library/Application Support/JetBrains/Toolbox"
 
 for config in .config/*; do
-    folder=${config##*/}
-    case "$folder" in
+    case "$(basename "$config")" in
         "raycast") for file in "$DOTFILES_ROOT/$config"/*; do symlink "$file" "$HOME/$config"; done ;;
         *) symlink "$DOTFILES_ROOT/$config" "$HOME/$config" ;;
     esac
