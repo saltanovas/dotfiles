2fdfaa5a1a13affa12dd60e20fb79d786a4572a7
saltanovas <zumbass@gmail.com>
2026-05-01 17:25:21 +0300

    Configure custom icons for VLC and Copilot

--- bootstrap.d/002-icns.sh
+++ bootstrap.d/002-icns.sh
@@ -2,20 +2,27 @@
 
 ICONS_HOME="$DOTFILES_ROOT/icns"
 
-if command -v fileicon >/dev/null; then
-    echo "Unable to set custom file icons: fileicon is not found"
+if ! command -v fileicon >/dev/null; then
+    echo "Unable to set custom app icons: fileicon is not found"
     exit 1
 fi
 
 typeset -A app_icons=(
-    "/Applications/Alacritty.app" "$ICONS_DIR/alacritty/alacritty-scanlines.png"
+    ["Alacritty.app"]="$ICONS_HOME/alacritty/alacritty-scanlines.png"
+    ["VLC.app"]="$ICONS_HOME/vlc.icns"
+    ["Copilot.app"]="$ICONS_HOME/copilot.icns"
 )
 
 # '!' returns keys, without it would return values
 for app in "${!app_icons[@]}"; do
-    [[ -d "$app" ]] || continue
+    appdir=""
+    for base in "/Applications" "$HOME/Applications"; do
+        [[ -d "$base/$app" ]] && appdir="$base/$app" && break
+    done
+
+    [[ -z "$appdir" ]] && continue
     icon="${app_icons[$app]}"
-    fileicon set "$app" "$icon"
+    fileicon set "$appdir" "$icon"
 done
 
 killall Dock
